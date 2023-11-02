4ba87005453daf59f233f29e42c62ef9bb32b2bd
ideaport2023 <kbaport2023@gmail.com>
2023-11-03 04:53:24 +0800

    add greet endpoint

--- src/main/java/dev/ideaport/hello/controller/HelloController.java
+++ src/main/java/dev/ideaport/hello/controller/HelloController.java
@@ -2,6 +2,7 @@ package dev.ideaport.hello.controller;
 
 import dev.ideaport.hello.model.Message;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class HelloController {
     @GetMapping
     public Message sayHello(){
         return new Message("Hello!");
+    }
+
+    @GetMapping("/{name}")
+    public Message greet(@PathVariable(value = "name") String name){
+        return new Message("Hello, " + name + "!");
     }
 }
